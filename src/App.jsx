3046514db943dd6b12814f98d9fffef6b5cf30b9
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-06-14 18:35:31 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { lazy } from "react";
+import './App.css'
 const Home = lazy(() => import("./pages/Home/Home"));
 // const Login = lazy(() => import("./pages/login/login"));
 function App() {
