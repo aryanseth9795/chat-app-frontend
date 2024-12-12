df5085a8c6688bfbfc854751a7713ff8fac1e42e
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-12-12 17:34:21 +0530

    Loader added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import "./App.css";
 import ProtectedRoute from "./components/Auth/ProtectedRoute";
+import { LayoutLoader } from "./components/Loader/Loader";
 const Home = lazy(() => import("./pages/Home/Home"));
 const NotFound = lazy(() => import("./pages/Not Found/NotFound"));
 const Login = lazy(() => import("./pages/Login/Login"));
@@ -12,11 +13,13 @@ function App() {
   return (
     <>
       <Router>
-        <Suspense fallback={<h1>Loader</h1>}>
+        <Suspense fallback={<LayoutLoader/>}>
           <Routes>
             <Route element={<ProtectedRoute user={user} Redirect="/login" />}>
               {/* All Protected ROutes come under this section */}
               <Route path="/" element={<Home />} />
+              <Route path="/chat:id" element={<Chat />} />
+              <Route path="/group" element={<Group />} />
             </Route>
             {/* // Now for login route */}
             <Route element={<ProtectedRoute user={!user} Redirect="/" />}>
