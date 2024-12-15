6170c5afc671fe471581257e9c8204e8e0b5c119
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-12-15 20:45:48 +0530

    Chatlist added

---
+++ src/components/Dialog/ChatList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatList() {
+  return (
+    <div>
+ Chatlist     
+    </div>
+  )
+}
+
+export default ChatList;
--- src/components/Layouts/Applayouts.jsx
+++ src/components/Layouts/Applayouts.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import Title from "../Common/title";
 import Header from "./Header";
-import {Grid} from '@mui/material'
-const AppLayout = () =>
-  (WrappedComponent) => {
-    return (props) => {
-        return (
+import { Grid } from "@mui/material";
+import ChatList from "../Dialog/chatList";
+const AppLayout = () => (WrappedComponent) => {
+  return (props) => {
+    return (
       <>
-      <Title/>
-        <Header/>
+        <Title />
+        <Header />
         <Grid container height={"calc(100vh-4rem)"}>
-
-
-
-            <Grid item xs={4} height={"100%"} >1</Grid>
-            <Grid item xs={4} height={"100%"} >
-        <WrappedComponent {...props} />
-              
-              </Grid>
-            <Grid item xs={4} height={"100%"} >
-3
-            </Grid>
-    
+          <Grid item xs={4} height={"100%"}>
+            <ChatList />
+          </Grid>
+          <Grid item xs={4} height={"100%"}>
+            <WrappedComponent {...props} />
+          </Grid>
+          <Grid item xs={4} height={"100%"}>
+            3
+          </Grid>
         </Grid>
-      </>)
-    };
+      </>
+    );
   };
+};
 
 export default AppLayout;
